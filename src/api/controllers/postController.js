9caccd437bcb4f70adfc6d714e3f088408359c77
Dormanus <39775844+Dormanus@users.noreply.github.com>
2020-11-24 10:03:26 +0100

    initialization
    Initialization project on git.

---
+++ src/api/controllers/postController.js
@@ -0,0 +1,78 @@
+const Post = require('../models/postModel');
+
+exports.list_all_posts = (req, res) => {
+    Post.find({}, (error, posts) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json(posts)
+        }
+    })
+}
+
+exports.create_a_post = (req, res) => {
+    let new_post = new Post(req.body);
+
+    new_post.save((error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(201);
+            res.json(post)
+        }
+    })
+}
+
+exports.get_a_post = (req, res) => {
+    Post.findById(req.params.post_id, (error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(201);
+            res.json(post)
+        }
+    })
+}
+
+exports.update_a_post = (req, res) => {
+    Post.findByIdAndUpdate(req.params.post_id, req.body, {new: true}, (error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json(post)
+        }
+    })
+}
+
+exports.delet_a_post = (req, res) => {
+    Post.findByIdAndRemove(req.params.post_id, (error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json({message: "Article supprimé."})
+        }
+    })
+}
